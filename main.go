5ccd21ee205536e93079f209b7139267a279bac8
Keith Kirtfield <keithkirtfield@Keiths-MBP.fios-router.home>
2020-04-14 20:37:00 -0400

    work

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+	Aim of this program -> ML Stock market guesser
+	Fetch data and place in pg_db
+	concurrently fetch and analyze data
+	Expose to outside world with gRPC
+	Host On AWS
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
